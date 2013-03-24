090e46492526d295ffb6b13cf1bf1021e9bb7c2b
Patrick McElhaney <pmcelhaney@gmail.com>
2013-03-24 14:45:02 -0400

    Added the simple winnning combinations (no diagonals)

--- 3d/generate-css.js
+++ 3d/generate-css.js
@@ -1,6 +1,8 @@
 var fs = require('fs');
 
 var turns = [];
+var winningCombinations = [];
+
 
 turns.push('label.if-o-goes-first { display: none; }');
 turns.push('#o-first:target label.if-o-goes-first { display: block; }');
@@ -18,4 +20,33 @@ for (var zIndex = 64; zIndex > 0; zIndex--) {
 
 }
 
+
+
+
+for (var i = 1; i <=4; i++) {
+	for (var j = 1; j <= 4; j++) {
+
+		// across (x axis)
+		var selector = '.y' + i + '.z' + j + ':checked';
+		winningCombinations.push('.o' + selector + ' ~ ' + '.o' + selector + ' ~ ' + '.o' + selector + ' ~ ' + '.o' + selector + ' ~ ' + '.if-o-wins');
+		winningCombinations.push('.x' + selector + ' ~ ' + '.x' + selector + ' ~ ' + '.x' + selector + ' ~ ' + '.x' + selector + ' ~ ' + '.if-x-wins');
+
+		// down (y axis)
+		selector = '.x' + i + '.z' + j + ':checked';
+		winningCombinations.push('.o' + selector + ' ~ ' + '.o' + selector + ' ~ ' + '.o' + selector + ' ~ ' + '.o' + selector + ' ~ ' + '.if-o-wins');
+		winningCombinations.push('.x' + selector + ' ~ ' + '.x' + selector + ' ~ ' + '.x' + selector + ' ~ ' + '.x' + selector + ' ~ ' + '.if-x-wins');
+
+		// in (z axis)
+		selector = '.x' + i + '.y' + j + ':checked';
+		winningCombinations.push('.o' + selector + ' ~ ' + '.o' + selector + ' ~ ' + '.o' + selector + ' ~ ' + '.o' + selector + ' ~ ' + '.if-o-wins');
+		winningCombinations.push('.x' + selector + ' ~ ' + '.x' + selector + ' ~ ' + '.x' + selector + ' ~ ' + '.x' + selector + ' ~ ' + '.if-x-wins');
+
+	}
+
+}
+
 fs.writeFile('turns.css', turns.join('\n'));
+
+console.log(winningCombinations.join(',\n'));
+
+
